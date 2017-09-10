d5d05942682a7bcc095b132fbe35347f67feed8c
Eloy Clemente <eloyclemente1@gmail.com>
2017-09-10 02:40:21 +0200

    No repeat

--- js/main.js
+++ js/main.js
@@ -45,5 +45,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
-	
+function outline()
+{
+	var allElements = document.getElementsByTagName('*');
+
+	for(var i=0; i < allElements.length; i++)
+	{
+		var elem = allElements[i];
+
+		elem.classList.toggle('outline');
+	}
+}
+document.getElementById('outline').addEventListener('click',outline);
+
 });
