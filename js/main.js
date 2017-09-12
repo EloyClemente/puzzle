86020d8bf3a5e36c1448b92fc5b2e5622e389878
Eloy Clemente <eloyclemente1@gmail.com>
2017-09-12 20:19:09 +0200

    Renombrar archivo js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 
-var container_origen  = document.getElementById('container_origen');
-var container_destino = document.getElementById('container_destino');
-var cantidad_casillas;
-var dificultad;
-var lista = ["bermeer", "chapelle"];
+const container_origen  = document.getElementById('container_origen')
+const container_destino = document.getElementById('container_destino')
 
-
-
-
-
-crear_capas("16"); // AL CARGAR INSERTA 16 CASILLAS POR DEFECTO
-
-
-
-
-
-// Buscar puzzle aleatorio
-function buscar_puzzle_aleatorio()
-{
-	var random = Math.random();
-	var indice_lista;
-	
-
-	if(random < 0.5)
-	{
-		indice_lista = 0;
-	}
-	else
-	{
-		indice_lista = 1;
-	}
-
-	var imagen = lista[indice_lista]
-
-	return imagen;
-}
-
-
-
-
-// Insertar puzzle aleatorio
-function insertar_puzzle_aleatorio()
-{
-	var puzzle = buscar_puzzle_aleatorio();
-
-	for(var i=0; i < 16; i++)
-	{
-		var casilla_ID = i + 1; // Sumo 1 para que coincida con el ID de la casilla
-		var casilla    = document.getElementById(casilla_ID);
-
-		casilla.innerHTML = "<img src='img/" +  puzzle + "/medium/" + (i+1) +".jpg'>"; // Sumo 1 para que coincida con el nombre de la imagen
-	}
-}
-insertar_puzzle_aleatorio();
+var numero_de_casillas
+var nombre_puzzle
 
 
 
@@ -65,7 +16,7 @@ insertar_puzzle_aleatorio();
 
 function limpiar_puzzle()
 {
-	container_origen.innerHTML = "";
+	container_origen.innerHTML = ""
 }
 
 
@@ -73,16 +24,128 @@ function limpiar_puzzle()
 
 
 
+// CREACIÓN DE RETÍCULA
+function crear_reticula(numero_de_casillas)
+{
+	limpiar_puzzle()
+
+
+	for(let i=0; i < numero_de_casillas; i++)
+	{
+		const capa_casilla = document.createElement('div')
+		capa_casilla.id    = i + 1 // Sumo 1 para que el 1er ID sea 1 en lugar de 0.
+
+
+				estilo_casillas(capa_casilla, numero_de_casillas)
+				container_origen.appendChild(capa_casilla) // Insertar casillas
+	}
+}
+crear_reticula("16")
+
+
+
+
+
+// ESTILO DE LA RETÍCULA
+function estilo_casillas(capa_casilla, numero_de_casillas)
+{
+	switch(numero_de_casillas)
+	{
+		case '9': 
+		capa_casilla.classList.add('casillas-9')
+		break
+
+		case '16':
+		capa_casilla.classList.add('casillas-16')
+		break
+
+		case '25':
+		capa_casilla.classList.add('casillas-25')
+		break
+	}
+}
+//***********************************************************
+
+
+
+
+
+// INSERTAR PUZZLE
+function insertar_puzzle( nombre_puzzle, numero_de_casillas)
+{
+	for(let i=0; i < numero_de_casillas; i++)
+	{
+		let casilla_ID    = i + 1  // Para que no comience por 0
+		let casilla       = document.getElementById(casilla_ID)
+
+
+		casilla.innerHTML = "<img src='img/" +  nombre_puzzle + "/" + numero_de_casillas + "/" + (i+1) + ".jpg'>"
+	}
+}
+//***********************************************************
+
+
+
+
+
+
+
+
+
+// BUSCAR PUZZLE ALEATORIO (adaptar el random al número de puzzles) !!!!
+function buscar_puzzle_aleatorio() // Es llamada en la siguiente función
+{
+	let indice_lista
+	const random = Math.random()
+	const lista  = ["bermeer", "chapelle"]
+	
+
+
+	if(random < 0.5)
+	{
+		indice_lista = 0
+	}
+	else
+	{
+		indice_lista = 1
+	}
+
+	nombre_puzzle = lista[indice_lista]
+
+	insertar_puzzle( nombre_puzzle, "16")
+}
+buscar_puzzle_aleatorio()
+//*************************************************************************
+
+
+
+
+
+
+// ASIGNAR EVENTO A LA LISTA DE PUZZLES
+function evento_desplegable()
+{
+	let selector_puzzle = document.getElementsByClassName('li-lista')
+
+	for(let i=0; i < selector_puzzle.length; i++)
+	{
+		selector_puzzle[i].addEventListener('click', enviar_puzzle)
+	}
+} evento_desplegable()
+
+
+
+
+
+
+// ASIGNAR EVENTO A LOS RADIO BUTTONS
 function identificar_radio_buttons()
 {
-	var radio_buttons = document.getElementsByClassName('radio');
+	let radio_buttons = document.getElementsByClassName('radio');
 
-	for(var i=0; i<radio_buttons.length; i++)
+	for(let i=0; i < 3; i++)
 	{
 		radio_buttons[i].addEventListener('change', obtener_data_radio_buttons);
-		radio_buttons[i].addEventListener('change', obtener_dificultad); // Para localizar la carpeta
-
-		// LLAMAR AQUÍ A LA FUNCIÓN QUE RESTAURA EL PUZZLE SELECCIONADO !!!!!!!!!!!!!!!!
 	}
 } identificar_radio_buttons();
 
@@ -91,34 +154,31 @@ function identificar_radio_buttons()
 
 
 
-// Obtiene las casillas del radio button
+// ENVIAR POR LISTA
+function enviar_puzzle(event)
+{
+	nombre_puzzle      = this.dataset.name; // Identificar el puzzle de la lista
+	let numero_de_casillas = container_origen.getElementsByTagName('div').length // Contar casillas
+
+	insertar_puzzle( nombre_puzzle, numero_de_casillas )
+}
+
+
+
+
+
+// DATA DE LOS RADIO BUTTONS
 function obtener_data_radio_buttons(event)
 {
-	cantidad_casillas = this.dataset.piezas;
+	let numero_de_casillas = this.dataset.piezas; // OBTIENE EL Nº DE CASILLAS ASIGNADO AL RADIO BUTTON
 
-	crear_capas(cantidad_casillas);
+	crear_reticula(numero_de_casillas)
+	insertar_puzzle( nombre_puzzle, numero_de_casillas )
 }
 
 
-
-
-
-
-// Crea las capas para las casillas
-function crear_capas(cantidad_casillas)
-{
-	limpiar_puzzle();
-
-
-	for(var i=0; i<cantidad_casillas ; i++)
-	{
-		var capa_casilla = document.createElement('div');
-		capa_casilla.id  = i + 1; // Sumo 1 para que el 1er ID sea 1 en lugar de 0.
-
-		estilo_casillas(capa_casilla, cantidad_casillas);
-		insertar_casillas(capa_casilla);
-	}
-}
+//*******************************************************************************************************
+//*******************************************************************************************************
 
 
 
@@ -126,96 +186,52 @@ function crear_capas(cantidad_casillas)
 
 
 
-function estilo_casillas(capa_casilla, cantidad_casillas)
-{
-	switch(cantidad_casillas)
-	{
-		case '9': 
-		capa_casilla.classList.add('piezas-9');
-		break;
-
-		case '16':
-		capa_casilla.classList.add('piezas-16');
-		break;
-
-		case '25':
-		capa_casilla.classList.add('piezas-25');
-		break;
-	}
-}
-
-
-
-
-
-function insertar_casillas(capa_casilla)
-{
-	container_origen.appendChild(capa_casilla);
-}
-
-
-//***********************************************************************
-
-
-
-
-function evento_desplegable() // Asignar evento a la lista de puzzles
-{
-	var selector_puzzle = document.getElementsByClassName('li-lista');
-
-	for(var i=0; i<selector_puzzle.length; i++)
-	{
-		selector_puzzle[i].addEventListener('click', obtener_puzzle);
-	}
-} evento_desplegable();
 
 
 
 
 
 
-// Para la ruta de la imagen
-function obtener_puzzle(event)
-{
-	var nombre_puzzle = this.dataset.name;
-
-	localizar_casillas(nombre_puzzle);
-}
-
-
-
-
-
-// Para la ruta de la imagen
-function obtener_dificultad(event) // "small", "medium", "big"
-{
-	dificultad = this.dataset.dificultad;
-}
 
 
 
 
 
 
-function localizar_casillas(nombre_puzzle)
-{
-	for(var i=0; i<cantidad_casillas; i++)
-	{
-		var casilla_ID = i + 1; // Sumo 1 para que coincida con el ID de la casilla
-		var casilla    = document.getElementById(casilla_ID);
-
-		insertar_imagen(casilla, nombre_puzzle, i);
-	}
-}
 
 
 
 
 
-function insertar_imagen(casilla, nombre_puzzle, i)
-{
-	casilla.innerHTML = "<img src='img/" +  nombre_puzzle + "/" + dificultad + "/" + (i+1) +".jpg'>"; // Sumo 1 para que coincida con el nombre de la imagen
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -255,11 +271,11 @@ function insertar_imagen(casilla, nombre_puzzle, i)
 
 function outline()
 {
-	var allElements = document.getElementsByTagName('*');
+	let allElements = document.getElementsByTagName('*');
 
-	for(var i=0; i < allElements.length; i++)
+	for(let i=0; i < allElements.length; i++)
 	{
-		var elem = allElements[i];
+		let elem = allElements[i];
 
 		elem.classList.toggle('outline');
 	}
